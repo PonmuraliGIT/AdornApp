0fae45a1128d79fbdf83bd70cdef0a0084dee7e9
Ponmurali <murali81rmspj@gmail.com>
2021-11-09 11:20:53 +0530

    This is my first commit

---
+++ orders/admin.py
@@ -0,0 +1,36 @@
+from django.contrib import admin
+from .models import Payment, Order, OrderProduct
+# Register your models here.
+
+
+class OrderProductInline(admin.TabularInline):
+    model = OrderProduct
+    readonly_fields = ('payment', 'user', 'product',
+                       'quantity', 'product_price', 'ordered')
+    extra = 0
+
+
+# class OrderProductAdmin(admin.ModelAdmin):
+#     model = OrderProduct
+#     list_display = ('payment', 'user', 'product',
+#                     'quantity', 'product_price', 'ordered')
+
+
+class OrderAdmin(admin.ModelAdmin):
+    list_display = ['order_number', 'full_name', 'phone', 'email',
+                    'city', 'order_total', 'tax', 'status', 'is_ordered', 'created_at']
+    list_filter = ['status', 'is_ordered']
+    search_fields = ['order_number', 'first_name',
+                     'last_name', 'phone', 'email']
+    list_per_page = 20
+    inlines = [OrderProductInline]
+
+
+class PaymentAdmin(admin.ModelAdmin):
+    list_display = ['payment_id', 'amount_paid', 'status', 'created_at']
+    list_per_page = 20
+
+
+admin.site.register(Payment, PaymentAdmin)
+admin.site.register(Order, OrderAdmin)
+admin.site.register(OrderProduct)
