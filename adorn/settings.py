35065ff93cc808d002806d79e5e0f6bb79183b29
Ponmurali <murali81rmspj@gmail.com>
2021-11-11 09:13:39 +0530

    testing

--- adorn/settings.py
+++ adorn/settings.py
@@ -96,10 +96,10 @@ AUTH_USER_MODEL = 'accounts.Account'
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
 # DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
+#      'default': {
+#          'ENGINE': 'django.db.backends.sqlite3',
+#          'NAME': BASE_DIR / 'db.sqlite3',
+#      }
 # }
 
 
