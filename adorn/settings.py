f033cfb834cb9d5b52f5050d8e65ae70d541747a
Ponmurali <murali81rmspj@gmail.com>
2021-11-16 06:45:57 +0530

    This is my first commit

--- adorn/settings.py
+++ adorn/settings.py
@@ -92,40 +92,17 @@ WSGI_APPLICATION = 'adorn.wsgi.application'
 AUTH_USER_MODEL = 'accounts.Account'
 
 
-# Database
-# https://docs.djangoproject.com/en/3.1/ref/settings/#databases
-
-# DATABASES = {
-#      'default': {
-#          'ENGINE': 'django.db.backends.sqlite3',
-#          'NAME': BASE_DIR / 'db.sqlite3',
-#      }
-# }
-
+ #Database
+ #https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'ebdb',
-        'USER': 'adornappuser',
-        'PASSWORD': 'simple123',
-        'HOST': 'aa1exdmsooubhe5.cdgs5n2hxll2.us-west-2.rds.amazonaws.com',
-        'PORT':5432,
-    }
+      'default': {
+          'ENGINE': 'django.db.backends.sqlite3',
+          'NAME': BASE_DIR / 'db.sqlite3',
+      }
 }
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.mysql',
-#         'NAME': 'adornDB',
-#         'HOST': '127.0.0.1',
-#         'PORT': '3306',
-#         'USER': 'root',
-#         'PASSWORD': '',
-#     }
-# }
-# Password validation
-# https://docs.djangoproject.com/en/3.1/ref/settings/#auth-password-validators
+
 
 AUTH_PASSWORD_VALIDATORS = [
     {
